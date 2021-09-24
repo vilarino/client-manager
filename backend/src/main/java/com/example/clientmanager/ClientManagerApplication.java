9f2499540948357b6bbddaa7aa88f3c2d2eceafe
Rafael Vilarino <rovilarino@gmail.com>
2021-09-24 09:07:33 -0300

    Repositories

---
+++ backend/src/main/java/com/example/clientmanager/ClientManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/example/clientmanager/repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.example.clientmanager.repositories;
+
+import com.example.clientmanager.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
